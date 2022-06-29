bb72a5cf7c0c4d1ec3fae1f4e4598f57d956d335
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-06-29 18:07:38 +0600

    Add files via upload

---
+++ Rihan_CL.py
@@ -0,0 +1,225 @@
+import os
+try:
+	import requests
+except:
+	os.system("pip install requests")
+import requests
+ 
+import os
+import sys
+import webbrowser
+import requests
+from threading import Thread
+import os
+import time
+
+# Reset
+color_off="\033[0m"       # Text Reset
+
+# Regular Colors
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+# Bold
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+
+# Underline
+ublack="\033[4;30m"       # Black
+ured="\033[4;31m"         # Red
+ugreen="\033[4;32m"       # Green
+uyellow="\033[4;33m"      # Yellow
+ublue="\033[4;34m"        # Blue
+upurple="\033[4;35m"      # Purple
+ucyan="\033[4;36m"        # Cyan
+uwhite="\033[4;37m"       # White
+
+# Background
+on_black="\033[40m"       # Black
+on_red="\033[41m"         # Red
+on_green="\033[42m"       # Green
+on_yellow="\033[43m"      # Yellow
+on_blue="\033[44m"        # Blue
+on_purple="\033[45m"      # Purple
+on_cyan="\033[46m"        # Cyan
+on_white="\033[47m"       # White
+
+# High Intensty
+iblack="\033[0;90m"       # Black
+ired="\033[0;91m"         # Red
+igreen="\033[0;92m"       # Green
+iyellow="\033[0;93m"      # Yellow
+iblue="\033[0;94m"        # Blue
+ipurple="\033[0;95m"      # Purple
+icyan="\033[0;96m"        # Cyan
+iwhite="\033[0;97m"       # White
+
+# Bold High Intensty
+biblack="\033[1;90m"      # Black
+bired="\033[1;91m"        # Red
+bigreen="\033[1;92m"      # Green
+biyellow="\033[1;93m"     # Yellow
+biblue="\033[1;94m"       # Blue
+bipurple="\033[1;95m"     # Purple
+bicyan="\033[1;96m"       # Cyan
+biehite="\033[1;97m"      # White
+
+# High Intensty backgrounds
+on_iblack="\033[0;100m"   # Black
+on_ired="\033[0;101m"     # Red
+on_igreen="\033[0;102m"   # Green
+on_iyellow="\033[0;103m"  # Yellow
+on_iblue="\033[0;104m"    # Blue
+on_ipurple="\033[10;95m"  # Purple
+on_icyan="\033[0;106m"    # Cyan
+on_iwhite="\033[0;107m"   # White
+
+
+
+
+os.system("clear")
+
+print (iblue+'''\n 
+
+$$$$$$$\  $$\ $$\                                  $$$$$$\  $$\       
+$$  __$$\ \__|$$ |                                $$  __$$\ $$ |      
+$$ |  $$ |$$\ $$$$$$$\   $$$$$$\  $$$$$$$\        $$ /  \__|$$ |      
+$$$$$$$  |$$ |$$  __$$\  \____$$\ $$  __$$\       $$ |      $$ |      
+$$  __$$< $$ |$$ |  $$ | $$$$$$$ |$$ |  $$ |      $$ |      $$ |      
+$$ |  $$ |$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |      $$ |  $$\ $$ |      
+$$ |  $$ |$$ |$$ |  $$ |\$$$$$$$ |$$ |  $$ |      \$$$$$$  |$$$$$$$$\ 
+\__|  \__|\__|\__|  \__| \_______|\__|  \__|       \______/ \________|
+                                                                      
+                                                                      
+                                                                      
+
+                
+'''+color_off)
+print(upurple+'''===================================================================================================='''+color_off)
+print(bgreen+'''			CEO- Bangladash Hacking Help Centre     '''                                   +color_off)
+print(bred+'''		CEO- Dark 420 Spamming Team     '''                              +color_off)
+print(bgreen+'''			Tools Developer- Rihan Ahmed    '''                            +color_off)
+
+
+print(upurple+'''================================================================'''+color_off)
+usern='''The'''
+passwd='''Rihan'''
+
+inpuser=str(input(byellow+'''\nEnter The Username: '''+color_off))
+inppass=str(input(byellow+'''Enter The Password: '''+color_off))
+
+if usern==inpuser and passwd==inppass:
+	print(igreen+'''[✓] Username & Password Correct! '''+color_off)
+	pass
+else:
+	print(bred+'''[ × ] Incorrect Username! /Password!
+                 Please Try Again
+
+                    [Contact Admin]  '''+color_off) 
+	os.system("xdg-open \'https://www.facebook.com/white.hat.hacker.Rihan\'")
+
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	os.system("python Rihan_CL.py")
+	sys.exit()
+	
+	url = "https://www.facebook.com/white.hat.hacker.Rihan/"
+
+	webbrowser.open(url, new=0, autoraise=True)
+
+os.system("clear")
+
+print (bpurple+'''\n 
+
+88""Yb 88 88  88    db    88b 88      dP""b8 88     
+88__dP 88 88  88   dPYb   88Yb88     dP   `" 88     
+88"Yb  88 888888  dP__Yb  88 Y88     Yb      88  .o 
+88  Yb 88 88  88 dP""""Yb 88  Y8      YboodP 88ood8 
+
+                
+'''+color_off)
+print(upurple+'''===================================================================================================='''+color_off)
+print(bgreen+'''	CEO- Bangladash Hacking Help Centre     '''                                   +color_off)
+print(bred+'''	CEO- Dark 420 Spamming Team     '''                              +color_off)
+print(bgreen+'''	Tools Developer- Rihan Ahmed    '''                            +color_off)
+print(upurple+'''================================================================'''+color_off)
+
+
+print('''\n [ 1 ] Facebook ID Super Cloning ''')
+print(''' [ 2 ] File 2009 / 2022 Cloning ''')
+print(''' [ 3 ] Facebook Cloning 2009 ''')
+print(''' [ 4 ] Facebook Cloning 2009 / 2015 ''')
+print(''' [ 5 ] Exit ''')
+
+print(yellow+'''            [ My Number: +8809638485201 ]              '''+color_off)
+
+(input(bcyan+'''\n[-] Select Your Option:>>> '''+color_off))
+
+os.system("clear")
+
+print(bpurple+'''
+                                                                    
+@@@@@@@   @@@  @@@  @@@   @@@@@@   @@@  @@@      @@@@@@@  @@@       
+@@@@@@@@  @@@  @@@  @@@  @@@@@@@@  @@@@ @@@     @@@@@@@@  @@@       
+@@!  @@@  @@!  @@!  @@@  @@!  @@@  @@!@!@@@     !@@       @@!       
+!@!  @!@  !@!  !@!  @!@  !@!  @!@  !@!!@!@!     !@!       !@!       
+@!@!!@!   !!@  @!@!@!@!  @!@!@!@!  @!@ !!@!     !@!       @!!       
+!!@!@!    !!!  !!!@!!!!  !!!@!!!!  !@!  !!!     !!!       !!!       
+!!: :!!   !!:  !!:  !!!  !!:  !!!  !!:  !!!     :!!       !!:       
+:!:  !:!  :!:  :!:  !:!  :!:  !:!  :!:  !:!     :!:        :!:      
+::   :::   ::  ::   :::  ::   :::   ::   ::      ::: :::   :: ::::  
+ :   : :  :     :   : :   :   : :  ::    :       :: :: :  : :: : :  
+                                                                    
+'''+color_off)
+
+print(ipurple+'''===================================================================================================='''+color_off)
+print(bgreen+		'''CEO- Bangladash Hacking Help Centre     '''                                   +color_off)
+print(bred+'''	CEO- Dark 420 Spamming Team     '''                              +color_off)
+print(bgreen+'''		Tools Developer- Rihan Ahmed    '''                            +color_off)
+print(upurple+'''================================================================'''+color_off)
+
+print(on_iblue+'''\n{Cloning Start Please Wait .... }'''+color_off)
+
+time.sleep(9)
+print(red+'''\n [ RIHAN - CP ] 100000640673884 | Ripon2233   '''+color_off)
+time.sleep(16)
+print(red+''' [ RIHAN - CP ] 100000235059602 | Modkha2233'''+color_off)
+time.sleep(7)
+print(igreen+''' [ RIHAN - OK ] 100082607507211 | Rayhan2233'''+color_off)
+time.sleep(6)
+print(red+''' [ RIHAN - CP ] 100000235059602 | Modkha2233'''+color_off)
+time.sleep(12)
+print(red+''' [ RIHAN - CP ] 100000355587582 | Motin2233'''+color_off)
+time.sleep(9)
+print(red+''' [ RIHAN - CP] 100000973330439 | Mofiz2233'''+color_off) 
+time.sleep(17)
+print(red+''' [ RIHAN - CP ] 100001301692255 | Nirob2233'''+color_off)
+time.sleep(15)
+print(igreen+'''  [ RIHAN - OK ] 100082477884517 | Shanto2233'''+color_off)
+time.sleep(8)
+print(red+''' [ RIHAN - CP ] 100001831360519 | Friyan2233'''+color_off)
+time.sleep(15)
+print(red+''' [ RIHAN - CP ] 100082477884577 | Sifat2233'''+color_off)
+time.sleep(8)
+print(red+''' [ RIHAN - CP ] 100001831360519 | Mukti2233'''+color_off)
+time.sleep(50)
+
